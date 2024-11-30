21a5720dba09e0db7e86da8ea6eda752e66b3469
Kudyakov Artem <t3m8ch@yandex.ru>
2024-11-30 14:03:25 +0400

    feat: Create db schema

--- db/CreateTables.sql
+++ db/CreateTables.sql
@@ -1,5 +1,7 @@
 CREATE TYPE user_role AS ENUM ('ADMIN', 'USER');
 
+CREATE TYPE poll_type AS ENUM ('FREE', 'RADIO');
+
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
     created_at TIMESTAMPTZ NOT NULL,
@@ -11,4 +13,30 @@ CREATE TABLE IF NOT EXISTS users (
     password_hash BYTEA NOT NULL,
     password_salt BYTEA NOT NULL,
     tg_link TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS polls (
+    id BIGSERIAL PRIMARY KEY,
+    text VARCHAR(300) NOT NULL,
+    type poll_type NOT NULL,
+    author_id BIGINT NOT NULL,
+    cluster INT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS user_answers (
+    user_id BIGINT NOT NULL,
+    poll_id BIGINT NOT NULL,
+    text VARCHAR(500) NOT NULL,
+    PRIMARY KEY (user_id, poll_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (poll_id) REFERENCES polls (id)
+);
+
+CREATE TABLE IF NOT EXISTS radio_answers (
+    answer_id BIGINT NOT NULL,
+    poll_id BIGINT NOT NULL,
+    text VARCHAR(500) NOT NULL,
+    PRIMARY KEY (answer_id, poll_id),
+    FOREIGN KEY (poll_id) REFERENCES polls (id)
 );
